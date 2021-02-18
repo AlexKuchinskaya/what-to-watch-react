25d17a2eb531de016a3e83b2bf082e757363ea1c
Alexandra <alexkuchynsk@gmail.com>
2021-02-18 13:40:06 +0100

    adding state for film-list

--- src/components/films-list/films-list.jsx
+++ src/components/films-list/films-list.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import MovieCard from '../movie-card/movie-card';
 import {filmsListPropTypes} from '../../types/types';
 
 const FilmList = (props) => {
   const {films} = props;
+
+  const [activeMovieCardId, setActiveMovieCardId] = useState();
+
+  const handleMovieSelect = (selectedMovieId) => {
+    setActiveMovieCardId(selectedMovieId);
+  };
+
   return (
     <div className="catalog__movies-list">
-      {films.map((film) => <MovieCard key={film.id} film={film}/>)}
+      {/* Active film id: {activeMovieCardId} */}
+      <br />
+      {films.map((film) => (
+        <MovieCard
+          key={film.id}
+          film={film}
+          onMovieSelect={handleMovieSelect}
+          isSelected={film.id === activeMovieCardId}
+        />
+      ))}
     </div>
   );
 };
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,32 +1,31 @@
-import React, {useState} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import {FilmPropType} from '../../types/types';
 
-const MovieCard = (props) => {
-  const {id, name, previewImage} = props.film;
+const MovieCard = ({film, onMovieSelect, isSelected}) => {
+  const {id, name, previewImage} = film;
 
-  const [activeMovieCardId, setActiveFilm] = useState({
-    id: 0
-  });
-  console.log([activeMovieCardId, setActiveFilm]);
-  const handleFilmMouseHover = (evt) => {
-    const SelectedMovieCardId = evt.currentTarget.id;
-    console.log(`SelectedMovieCardId`, SelectedMovieCardId)
-    // setActiveFilm();
+  const handleFilmMouseHover = () => {
+    onMovieSelect(id);
   };
-  return <>;
-    <article onMouseOver={(handleFilmMouseHover)} className="small-movie-card catalog__movies-card" id={id}>
+
+  return (
+    <article onMouseOver={handleFilmMouseHover} className="small-movie-card catalog__movies-card">
       <div className="small-movie-card__image">
         <img src={previewImage} alt={name} width="280" height="175" />
       </div>
       <h3 className="small-movie-card__title">
         <a className="small-movie-card__link" href="movie-page.html">{name}</a>
       </h3>
+      { isSelected ? `Selected` : `` }
     </article>
-  </>;
+  );
 };
 
 MovieCard.propTypes = {
-  film: FilmPropType
+  film: FilmPropType,
+  onMovieSelect: PropTypes.func.isRequired,
+  isSelected: PropTypes.bool
 };
 
 export default MovieCard;
